e2d5b74655d9f19c93b5e9aa1c04ed350e47a84e
Abyan Daroesman <114374540+hexythegreat@users.noreply.github.com>
2023-03-14 13:27:53 +0700

    Add files via upload

---
+++ pertemuan3/tugas/home.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* Task 1
+* Pisah kode di bawah ke dalam file-file yang sudah dibuat sebelumnya.
+*/
+?>
+
+<body>
+    <div class="container mt-5">
+        <?php 
+		include_once 'header.php' 
+		?>
+		<h1>Welcome Home!</h1>
+		<p>
+		 Abyan Daroesman 0110222063 TI04
+		</p>
+		<?php 
+		include_once 'footer.php' 
+		?>
+    </div>
+</body>
+
+</html>
